cb26d4a753db38b20a798ab9877a5338c283e0aa
FabriceIRANKUNDA <irankundafabrice8@gmail.com>
2021-07-10 14:29:28 +0200

    Implemented meal items

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import headerImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={headerImage} alt="header cover" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/Components/UI/Modal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import classes from "./Modal.module.css";
+
+const BackDrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div classes={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  const portalElement = document.getElementById("overlays");
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<BackDrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Modal;
